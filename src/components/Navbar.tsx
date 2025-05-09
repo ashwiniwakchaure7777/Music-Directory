f0f74f6c9a0f008eefb36018994576649b77496d
Ashwini <ashwini@phantasm.co.in>
2025-05-09 18:00:57 +0530

    Initial commit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,26 @@
-'use client'
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
-const Navbar = () => {
+const Navbar = ({ className }: { className?: string }) => {
+  const [active, setActive] = useState<string | null>(null);
   return (
-    <div>Navbar</div>
-  )
-}
+    <div
+      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <Link href={"/"}>
+          <MenuItem
+            setActive={setActive}
+            active={active}
+            item="Home"
+          ></MenuItem>
+        </Link>
+      </Menu>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
