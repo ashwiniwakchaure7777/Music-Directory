1480c394034088690903a27246c0b860bdd071ac
Ashwini <ashwini@phantasm.co.in>
2025-05-12 10:22:58 +0530

    Changes in the card

--- src/components/TestimonialCards.tsx
+++ src/components/TestimonialCards.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { InfiniteMovingCards } from "./ui/infinite-moving-cards";
+import { cn } from "@/lib/utils";
 
 const musicSchoolTestimonials = [
   {
@@ -37,10 +38,19 @@ const musicSchoolTestimonials = [
 const MusicSchoolTestimonials = () => {
   return (
     <div className="h-[40rem] w-full dark:bg-black dark:bg-grid-white/[0.2] relative flex flex-col items-center justify-center overflow-hidden">
+      <div
+        className={cn(
+          "absolute inset-0",
+          "[background-size:40px_40px]",
+          "[background-image:linear-gradient(to_right,#e4e4e7_1px,transparent_1px),linear-gradient(to_bottom,#e4e4e7_1px,transparent_1px)]",
+          "dark:[background-image:linear-gradient(to_right,#262626_1px,transparent_1px),linear-gradient(to_bottom,#262626_1px,transparent_1px)]"
+        )}
+      />
       <h2 className="text-3xl font-bold text-center mb-8 z-10">
         Hear Our Harmony: Voices of success
       </h2>
       <div className="flex justify-center w-full overflow-hidden px-4 sm:px-6 lg:px-8">
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)] dark:bg-black"></div>
         <div className="w-full max-w-6xl">
           <InfiniteMovingCards
             items={musicSchoolTestimonials}
